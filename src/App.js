fdb94fe1dda38be0693fc58ea66b3c17a8d72a24
Folami Oluwapelumi <folamioluwapelumi1@gmail.com>
2022-08-20 00:00:18 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Homepage from './Homepage.jsx';
+import './styles/app.scss'
+
+function App() {
+  return (
+    <div className='App'>
+      <Homepage/>  
+    </div>
+  );
+}
+
+export default App;
